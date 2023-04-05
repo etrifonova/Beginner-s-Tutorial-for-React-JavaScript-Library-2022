bd4b8bf0b39822a70f17ed54313df4b2e8ef14f5
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-04-05 15:18:33 +0300

    feat: revise thought experiment

--- 06_Thought Experiment/index.js
+++ 06_Thought Experiment/index.js
@@ -9,7 +9,10 @@ const catProfile = (
     </div>
 )
 
+// document.getElementById("root").append(catProfile);
 // document.getElementById("root").append(JSON.stringify(catProfile));
+/*{"type":"div","key":null,"ref":null,"props":{"children":[{"type":"h1","key":null,"ref":null,"props":{"children":"Sima's Best Qualities"},"_owner":null,"_store":{}},{"type":"ul","key":null,"ref":null,"props":{"children":[{"type":"li","key":null,"ref":null,"props":{"children":"Fluffiness"},"_owner":null,"_store":{}},{"type":"li","key":null,"ref":null,"props":{"children":"Calmness"},"_owner":null,"_store":{}},{"type":"li","key":null,"ref":null,"props":{"children":"Beauty"},"_owner":null,"_store":{}}]},"_owner":null,"_store":{}}]},"_owner":null,"_store":{}}
+ */ 
 // // what is displayed: [object Object]
 // //JSX returns plain JS objects
 
