acedd8b33b2fcdbd62c1a8816fb67e5d6c4ae469
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-04-07 14:03:55 +0300

    feat: learn export & import

---
+++ 11_React Info Site - Building Components/Header.js
@@ -0,0 +1,18 @@
+// the file name starts with a capital letter
+
+function Header() {
+  return (
+    <header>
+      <nav className="navigation">
+        <img src="./react-logo.png" alt="React Logo" className="nav-img" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
