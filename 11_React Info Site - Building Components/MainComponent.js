afe479ee67caa967c0cd0076fbcc4e46c0c3324f
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-04-18 17:06:42 +0300

    feat: modify w/out success

---
+++ 11_React Info Site - Building Components/MainComponent.js
@@ -0,0 +1,15 @@
+// the file name starts with a capital letter
+
+export default function MainComponent() {
+  return (
+    <div className="main-container">
+      <h1>Reasons to learn React</h1>
+        <ol>
+          <li>It facilitates website building</li>
+          <li>It's a hirable skill</li>
+          <li>It's awesome</li>
+        </ol>
+    </div>
+  )
+}
+
